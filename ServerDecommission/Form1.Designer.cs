cb427648b23689bce4b85b37aa8e2ab6b4eac895
Richard <rahayn@yahoo.com>
2017-12-06 16:16:34 -0500

    Initial commit

---
+++ ServerDecommission/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace ServerDecommission
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.make_server_backup = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(110, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(326, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Server Decommission Tool";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Enter Name";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(256, 144);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(308, 23);
+            this.progressBar1.TabIndex = 4;
+            this.progressBar1.UseWaitCursor = true;
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(116, 84);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(464, 84);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(299, 87);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(149, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Enter Incident Number";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(16, 198);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(200, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Stop Services";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(364, 198);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(200, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Restart Services";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // make_server_backup
+            // 
+            this.make_server_backup.Location = new System.Drawing.Point(16, 143);
+            this.make_server_backup.Name = "make_server_backup";
+            this.make_server_backup.Size = new System.Drawing.Size(200, 23);
+            this.make_server_backup.TabIndex = 10;
+            this.make_server_backup.Text = "Backup Server";
+            this.make_server_backup.UseVisualStyleBackColor = true;
+            this.make_server_backup.Click += new System.EventHandler(this.make_server_backup_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(576, 531);
+            this.Controls.Add(this.make_server_backup);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button make_server_backup;
+    }
+}
+
---
+++ ServerDecommission/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using Microsoft.SqlServer.Management.Smo;
+using Microsoft.SqlServer.Management.Common;
+/*
+Functions
+•	Enter data before start
+o	Enter in the name of the tech doing the move
+o	Enter the ticket number
+o	Log
+•	Note
+o	Allow the tech an area for notes
+•	Make SQL backup of Pizza database
+o	Path: C:\srv\backup
+o	Filename: ServerMoveMMDDYY.bak
+o	Open Explorer
+o	Log 
+•	Change computer name
+o	ServerName to ServerName-Old (what ever the server name is, just add -Old)
+o	Log
+•	Change IP Address
+o	Check to see if 10.0.0.111 is available
+o	Change to an open 111 to 115
+o	Log
+•	Windows Services
+o	Stop all the services 
+o	Disable all the services
+o	C# Config file holds the list
+o	List
+	Apache 2.2 
+	CRSWebCron 
+	LCE Store Digital Order Priority Dispatcher
+	LCE Store Systems Digital Order Feed Service
+	LCE Store Systems Digital Order HNR Portal Service
+	SQL Server Express Database (Service Name: SQL Server (DATABASENAME)
+	Rabbit MQ
+o	Log
+•	Windows Scheduled Task
+o	Disable all the task
+o	C# Config file holds the list
+o	List
+	ESB CreateRabbitMQConfig
+	ESB HeartBeat
+	ESB StoreSalesSummary
+	BD Backup (optional)
+o	Log
+•	Log
+o	Tech name
+o	Ticket number
+o	Computer name
+o	IP Address
+o	Date Time Stamp
+o	Log each of the functions begin done with a:
+	Start of procedure
+	End of procedure with complete or not
+o	Email the log (supervisor)
+	C# Config file holds the email(s)
+	Note attached
+o	Copy and paste button
+	Note attached
+o	On closing of program
+	Note attached
+	Save log to c:\srv\backup
+	Filename: ServerMoveMMDDYY_HHMMSS.txt
+•	Reboot Server
+o	Dont allow the reboot from the program until the log is emailed
+
+*/
+namespace ServerDecommission
+{
+    public partial class Form1 : Form
+    {   
+        //SqlConnection con = new SqlConnection()
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void make_server_backup_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+            progressBar1.Value = 0;
+            try
+            {
+                Server dbServer = new Server();
+                Backup pizzaBackup = new Backup() { Action = BackupActionType.Database };
+                //TODO db name for file and path needs to be added as well as backup completion 
+                pizzaBackup.Devices.AddDevice(@"C:\srv\backup\BackupForServerMove.bak",DeviceType.File);
+                pizzaBackup.Initialize = true;
+                pizzaBackup.PercentComplete += pizzaBackup_PercentComplete;
+                pizzaBackup.Complete += pizzaBackup_Complete;
+                pizzaBackup.SqlBackupAsync(dbServer);
+
+
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void pizzaBackup_PercentComplete(object snder, ServerMessageEventArgs e)
+        {   
+            //TODO Exception needs a mean by which to determine if there is a failure
+            if(e.Error != null)
+            {
+                
+            }
+
+        }
+        private void pizzaBackup_Complete(object snder, ServerMessageEventArgs e)
+        {
+
+        }
+    }
+}
