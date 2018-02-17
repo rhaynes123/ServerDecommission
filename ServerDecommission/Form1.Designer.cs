c6712d42ec25cb797b5f130c39955fecaadd63f9
Richard Haynes <Richard.Haynes@lcecorp.com>
2018-02-16 19:34:18 -0500

    uichanges
    made several changes to the winform

--- ServerDecommission/Form1.Designer.cs
+++ ServerDecommission/Form1.Designer.cs
@@ -30,13 +30,15 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.progressBar1 = new System.Windows.Forms.ProgressBar();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.nameEntered = new System.Windows.Forms.TextBox();
+            this.incidentNumberEntered = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             this.make_server_backup = new System.Windows.Forms.Button();
+            this.stopScheduledTasks = new System.Windows.Forms.Button();
+            this.ScanForServerIP = new System.Windows.Forms.Button();
+            this.final_restart_server = new System.Windows.Forms.Button();
+            this.renamebutton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -59,28 +61,25 @@
             this.label2.TabIndex = 1;
             this.label2.Text = "Enter Name";
             // 
-            // progressBar1
+            // nameEntered
             // 
-            this.progressBar1.Location = new System.Drawing.Point(256, 144);
-            this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(308, 23);
-            this.progressBar1.TabIndex = 4;
-            this.progressBar1.UseWaitCursor = true;
-            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+            this.nameEntered.AccessibleName = "nameEntered";
+            this.nameEntered.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.nameEntered.Location = new System.Drawing.Point(116, 84);
+            this.nameEntered.Name = "nameEntered";
+            this.nameEntered.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.nameEntered.Size = new System.Drawing.Size(100, 22);
+            this.nameEntered.TabIndex = 5;
+            this.nameEntered.TextChanged += new System.EventHandler(this.nameEntered_TextChanged);
             // 
-            // textBox1
+            // incidentNumberEntered
             // 
-            this.textBox1.Location = new System.Drawing.Point(116, 84);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 22);
-            this.textBox1.TabIndex = 5;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(464, 84);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 22);
-            this.textBox2.TabIndex = 6;
+            this.incidentNumberEntered.AccessibleName = "incidentEntered";
+            this.incidentNumberEntered.Location = new System.Drawing.Point(464, 84);
+            this.incidentNumberEntered.Name = "incidentNumberEntered";
+            this.incidentNumberEntered.Size = new System.Drawing.Size(100, 22);
+            this.incidentNumberEntered.TabIndex = 6;
+            this.incidentNumberEntered.TextChanged += new System.EventHandler(this.incidentNumberEntered_TextChanged);
             // 
             // label4
             // 
@@ -93,25 +92,19 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(16, 198);
+            this.button1.Location = new System.Drawing.Point(179, 201);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(200, 23);
             this.button1.TabIndex = 8;
-            this.button1.Text = "Stop Services";
+            this.button1.Text = "Open Services";
+            this.button1.UseMnemonic = false;
             this.button1.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(364, 198);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(200, 23);
-            this.button2.TabIndex = 9;
-            this.button2.Text = "Restart Services";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // make_server_backup
             // 
-            this.make_server_backup.Location = new System.Drawing.Point(16, 143);
+            this.make_server_backup.AccessibleName = "backupbutton";
+            this.make_server_backup.Location = new System.Drawing.Point(179, 143);
             this.make_server_backup.Name = "make_server_backup";
             this.make_server_backup.Size = new System.Drawing.Size(200, 23);
             this.make_server_backup.TabIndex = 10;
@@ -119,21 +112,67 @@
             this.make_server_backup.UseVisualStyleBackColor = true;
             this.make_server_backup.Click += new System.EventHandler(this.make_server_backup_Click);
             // 
+            // stopScheduledTasks
+            // 
+            this.stopScheduledTasks.Location = new System.Drawing.Point(179, 252);
+            this.stopScheduledTasks.Name = "stopScheduledTasks";
+            this.stopScheduledTasks.Size = new System.Drawing.Size(200, 23);
+            this.stopScheduledTasks.TabIndex = 12;
+            this.stopScheduledTasks.Text = "Open Scheuled Tasks";
+            this.stopScheduledTasks.UseVisualStyleBackColor = true;
+            this.stopScheduledTasks.Click += new System.EventHandler(this.stopScheduledTasks_Click);
+            // 
+            // ScanForServerIP
+            // 
+            this.ScanForServerIP.Location = new System.Drawing.Point(179, 301);
+            this.ScanForServerIP.Name = "ScanForServerIP";
+            this.ScanForServerIP.Size = new System.Drawing.Size(200, 23);
+            this.ScanForServerIP.TabIndex = 14;
+            this.ScanForServerIP.Text = "Scan For Server IP";
+            this.ScanForServerIP.UseVisualStyleBackColor = true;
+            this.ScanForServerIP.Click += new System.EventHandler(this.ScanForServerIPClick);
+            // 
+            // final_restart_server
+            // 
+            this.final_restart_server.Location = new System.Drawing.Point(195, 389);
+            this.final_restart_server.Name = "final_restart_server";
+            this.final_restart_server.Size = new System.Drawing.Size(165, 23);
+            this.final_restart_server.TabIndex = 16;
+            this.final_restart_server.Text = "Restart Server";
+            this.final_restart_server.UseVisualStyleBackColor = true;
+            this.final_restart_server.Click += new System.EventHandler(this.final_restart_server_Click);
+            // 
+            // renamebutton
+            // 
+            this.renamebutton.Location = new System.Drawing.Point(179, 346);
+            this.renamebutton.Name = "renamebutton";
+            this.renamebutton.Size = new System.Drawing.Size(200, 23);
+            this.renamebutton.TabIndex = 17;
+            this.renamebutton.Text = "Rename Server";
+            this.renamebutton.UseVisualStyleBackColor = true;
+            this.renamebutton.Click += new System.EventHandler(this.renamebutton_Click);
+            // 
             // Form1
             // 
+            this.AccessibleRole = System.Windows.Forms.AccessibleRole.MenuBar;
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.OrangeRed;
             this.ClientSize = new System.Drawing.Size(576, 531);
+            this.Controls.Add(this.renamebutton);
+            this.Controls.Add(this.final_restart_server);
+            this.Controls.Add(this.ScanForServerIP);
+            this.Controls.Add(this.stopScheduledTasks);
             this.Controls.Add(this.make_server_backup);
-            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.incidentNumberEntered);
+            this.Controls.Add(this.nameEntered);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
             this.Text = "Form1";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -144,13 +183,15 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.ProgressBar progressBar1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox nameEntered;
+        private System.Windows.Forms.TextBox incidentNumberEntered;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button make_server_backup;
+        private System.Windows.Forms.Button stopScheduledTasks;
+        private System.Windows.Forms.Button ScanForServerIP;
+        private System.Windows.Forms.Button final_restart_server;
+        private System.Windows.Forms.Button renamebutton;
     }
 }
 
--- ServerDecommission/Form1.cs
+++ ServerDecommission/Form1.cs
@@ -1,15 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net.NetworkInformation;
 using System.Windows.Forms;
 using System.Data.SqlClient;
-using Microsoft.SqlServer.Management.Smo;
 using Microsoft.SqlServer.Management.Common;
+using System.Diagnostics;
+using System.IO;
+
+//using LCE.StoreSystems.Common;
 /*
 Functions
 •	Enter data before start
@@ -27,8 +24,8 @@ o	Log
 o	ServerName to ServerName-Old (what ever the server name is, just add -Old)
 o	Log
 •	Change IP Address
-o	Check to see if 10.0.0.111 is available
-o	Change to an open 111 to 115
+o	Check to see if 10.0.0.11 is available
+o	Change to an open 11 to 115
 o	Log
 •	Windows Services
 o	Stop all the services 
@@ -77,8 +74,8 @@ o	Dont allow the reboot from the program until the log is emailed
 namespace ServerDecommission
 {
     public partial class Form1 : Form
-    {   
-        //SqlConnection con = new SqlConnection()
+    {
+        
         public Form1()
         {
             InitializeComponent();
@@ -86,43 +83,193 @@ namespace ServerDecommission
 
         private void make_server_backup_Click(object sender, EventArgs e)
         {
-            
-        }
-
-        private void progressBar1_Click(object sender, EventArgs e)
-        {
-            progressBar1.Value = 0;
+           
             try
             {
-                Server dbServer = new Server();
-                Backup pizzaBackup = new Backup() { Action = BackupActionType.Database };
-                //TODO db name for file and path needs to be added as well as backup completion 
-                pizzaBackup.Devices.AddDevice(@"C:\srv\backup\BackupForServerMove.bak",DeviceType.File);
-                pizzaBackup.Initialize = true;
-                pizzaBackup.PercentComplete += pizzaBackup_PercentComplete;
-                pizzaBackup.Complete += pizzaBackup_Complete;
-                pizzaBackup.SqlBackupAsync(dbServer);
+                string connectionString = "Server=127.0.0.1;Database=pizza;Connection Timeout=120;Trusted_Connection=Yes;";
+               
+                SqlConnection dbconnection = new SqlConnection(connectionString);
 
+                SqlCommand sqlcmd = new SqlCommand();
 
+                SqlDataReader reader;
+
+                sqlcmd.CommandText = "Backup database pizza to disk = 'C:\\srv\\backup\\ServerMove.bak'with format; go";//TODO insert TSQL backup query
+
+                sqlcmd.CommandType = System.Data.CommandType.Text;
+
+                sqlcmd.Connection = dbconnection;
+
+                dbconnection.Open();
+
+                reader = sqlcmd.ExecuteReader();
+
+                dbconnection.Close();
+
+                //TODO db name for file and path needs to be added as well as backup completion
+                //c:\BDBackup\BDbackup.cmd
+                /*
+                 try
+                 {
+                    Process.Start("BDBackup.cmd");
+                 }
+
+                catch(Exception ex)
+                {
+                    MessageBox.Show("Error: Failure Running the Back Up ", ex.Message)
+                }
+                 */
+                //TODO back failed 1/18/2018 
 
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
+            
         }
-        private void pizzaBackup_PercentComplete(object snder, ServerMessageEventArgs e)
-        {   
-            //TODO Exception needs a mean by which to determine if there is a failure
-            if(e.Error != null)
+
+        private void button1_Click(object sender, EventArgs e)
+        {  
+            try
             {
-                
+                var services = "services.msc";
+                Process.Start(services);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("No windows Services Found.\n {0}",ex.Message);
+            }
+            
+        }
+
+        private void final_restart_server_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Process.Start("shutdown", " / r / t 0");
+            }
+           catch(Exception ex)
+            {
+                MessageBox.Show("Currently Unable to Shutdown and Restart.\n {0}", ex.Message);
+            }
+        }
+
+        //Checks for the STANDARD IP Address Scheme and Default Gateway
+        private void ScanForServerIPClick(object sender, EventArgs e)
+        {
+            try
+            {
+
+                var message = string.Empty;
+                var error = false;
+
+                //Runs a Check to Confirm if the standard server address can be used.
+                Ping scanforping = new Ping();
+
+                PingReply ipaddressreply = scanforping.Send("10.0.0.11");
+                //Double checks to make sure if the default gateway is also standard.
+                Ping defaultGatewayPing = new Ping();
+
+                PingReply Gatewayreply = defaultGatewayPing.Send("10.0.0.1");
+                // string.Compare(Gatewayreply.Status, "Success", true);
+
+                if (Gatewayreply.Status == IPStatus.Success)
+                {
+                    message = "10.0.0.11 is in use. Please Exit";
+                }
+                else if (Gatewayreply.Status == IPStatus.DestinationHostUnreachable
+                    || Gatewayreply.Status == IPStatus.BadDestination)
+                {
+                    error = true;
+                    message = "ERROR: SITE IS ON NON-STANDARD SCHEME: SERVER IP ADDRESS CAN'T BE USED.";
+
+                }
+                else
+                {
+                    error = true;
+                    message = string.Format("Standard IP not Found: Reason Status {0}", Gatewayreply.Status);
+                }
+
+                MessageBox.Show(message, error ? "Error" : "Information", MessageBoxButtons.OK, error ? MessageBoxIcon.Error : MessageBoxIcon.Information);
+
+                if (error)
+                {
+                    return;
+                }
             }
 
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
-        private void pizzaBackup_Complete(object snder, ServerMessageEventArgs e)
-        {
 
+        private void stopScheduledTasks_Click(object sender, EventArgs e)
+        {
+            //TODO Stops the Windows Task
+            try
+            {
+                var taskScheduler = "taskschd.msc";
+                Process.Start(taskScheduler);
+            }
+            catch(Exception ex)
+            {
+               MessageBox.Show("Unable to open the Task Scheduler.\n {0}", ex.Message);
+            }
+            
+        }
+
+        public void nameEntered_TextChanged(object sender, EventArgs e)
+        {
+            //Entered Name
+            var enteredName = nameEntered.Text.Trim();
+        }
+            
+        public void incidentNumberEntered_TextChanged(object sender, EventArgs e)
+        {   
+            //Entered Incident
+            var incidentNumber = incidentNumberEntered.Text.Trim();
+        }
+        public void logConfigFilePath()
+        {
+            //Writes the Log file and reads from the config file.
+
+            var logMessage = "decommisioned this server on date. Please reference incident number: ";
+            var logFilename = "decommissionlog.txt";
+
+            DirectoryInfo[] folderPath = new DirectoryInfo(@"c:\srv\backups").GetDirectories();
+            //Write to log file
+            try
+            {
+                using (StreamWriter writeLog = new StreamWriter(logFilename))
+                {
+                    writeLog.WriteLine("{0},{1},{2}", nameEntered, logMessage, incidentNumberEntered);
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Error Unable to Update Folder Path {0}", ex.Message);
+                
+            }
+            finally
+            {
+               //TODO Close StreamWrite
+            }
+           
+        }
+
+        private void renamebutton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var rename = "SystemPropertiesComputerName.exe";
+                Process.Start(rename);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Unable to access the Hostname settings.\n {0}", ex.Message);
+            }
         }
     }
 }
